53b63132128a1d8e38f3af00f2a81c1b99a3724c
ga1duk <gaidukdg@yandex.ru>
2021-09-21 17:13:20 +0300

    initial commit

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,11 +1,11 @@
-package ru.netology.nmedia
+package ru.netology.nmedia.dto
 
 data class Post(
     val id: Long,
     val author: String,
     val content: String,
     val published: String,
-    var likedByMe: Boolean = false,
+    val likedByMe: Boolean = false,
     var likesCount: Int = 999,
     var sharesCount: Int = 10_999
 )
