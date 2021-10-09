f248a03c84421c99d015afec6794bc124fd24be1
ga1duk <gaidukdg@yandex.ru>
2021-10-10 02:53:24 +0300

    app architecture was modified according to MVVM principles

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,38 +1,33 @@
 package ru.netology.nmedia.activity
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import ru.netology.nmedia.dto.Post
-import ru.netology.nmedia.R
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.R
 import ru.netology.nmedia.convertNumber
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val post = Post(
-//            id = 1,
-//            author = "Нетология. Университет интернет-профессий будущего",
-//            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-//            published = "21 мая в 18:36"
-//        )
-
         val viewModel: PostViewModel by viewModels()
-
+//        Подписываемся на обновление data во viewModel
         viewModel.data.observe(this) { post ->
-            binding.tvAuthor.text = post.author
-            binding.tvPublished.text = post.published
-            binding.tvContent.text = post.content
-            binding.tvLikes.text = convertNumber(post.likesCount)
-            binding.tvShares.text = convertNumber(post.sharesCount)
-            if (post.likedByMe) {
-                binding.ivLikes.setImageResource(R.drawable.ic_baseline_favorite_24)
+            with(binding) {
+                tvAuthor.text = post.author
+                tvPublished.text = post.published
+                tvContent.text = post.content
+                tvLikes.text = convertNumber(post.likesCount)
+                tvShares.text = convertNumber(post.sharesCount)
+                ivLikes.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_baseline_favorite_24
+                    else R.drawable.ic_baseline_favorite_border_24
+                )
             }
         }
 
@@ -40,21 +35,8 @@ class MainActivity : AppCompatActivity() {
             viewModel.like()
         }
 
-            binding.ivLikes.setOnClickListener {
-                if (!post.likedByMe) {
-                    binding.ivLikes.setImageResource(R.drawable.ic_baseline_favorite_24)
-                    post.likesCount++
-                } else {
-                    binding.ivLikes.setImageResource(R.drawable.ic_baseline_favorite_border_24)
-                    post.likesCount--
-                }
-                binding.tvLikes.text = convertNumber(post.likesCount)
-                post.likedByMe = !post.likedByMe
-
-            binding.ivShares.setOnClickListener {
-                post.sharesCount++
-                binding.tvShares.text = convertNumber(post.sharesCount)
-            }
+        binding.ivShares.setOnClickListener {
+            viewModel.share()
         }
     }
 }
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -11,11 +11,21 @@ class PostRepositoryInMemoryImpl : PostRepository {
         content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
         published = "21 мая в 18:36"
     )
+
     private val data = MutableLiveData(post)
 
     override fun get(): LiveData<Post> = data
     override fun like() {
-        post = post.copy(likedByMe = !post.likedByMe)
+        post = post.copy(
+            likedByMe = !post.likedByMe,
+            likesCount = if (!post.likedByMe) post.likesCount + 1 else post.likesCount - 1
+        )
+//        изменяем объект data с помощью функции value
+        data.value = post
+    }
+
+    override fun share() {
+        post = post.copy(sharesCount = post.sharesCount + 1)
         data.value = post
     }
 }
