50b21a7bbd7b6852af2f6750efd651eec229688c
ga1duk <gaidukdg@yandex.ru>
2021-10-19 19:14:16 +0300

    app was modified by adding recyclerView. Push on ivLikes is handled

---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -0,0 +1,53 @@
+package ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+
+typealias OnLikeListener = (post: Post) -> Unit
+
+class PostsAdapter(private val onLikeListener: OnLikeListener) :
+    RecyclerView.Adapter<PostViewHolder>() {
+    var list = emptyList<Post>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, onLikeListener)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post = list[position]
+        holder.bind(post)
+    }
+
+    override fun getItemCount(): Int = list.size
+}
+
+class PostViewHolder(
+    private val binding: CardPostBinding,
+    private val onLikeListener: OnLikeListener
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(post: Post) {
+        binding.apply {
+            tvAuthor.text = post.author
+            tvPublished.text = post.published
+            tvContent.text = post.content
+//            tvLikes.text = convertNumber(post.likesCount)
+//                    tvShares.text = convertNumber(post.sharesCount)
+            ivLikes.setImageResource(
+                if (post.likedByMe) R.drawable.ic_baseline_favorite_24
+                else R.drawable.ic_baseline_favorite_border_24
+            )
+            ivLikes.setOnClickListener {
+                onLikeListener(post)
+            }
+        }
+    }
+}
